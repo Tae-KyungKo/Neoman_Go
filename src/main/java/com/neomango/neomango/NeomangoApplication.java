dc1dc50801fed75969695ea15d3f189d8ff0dd10
ttt103 <ttt103tk@gmail.com>
2026-04-26 18:53:28 +0900

    chore: Create project file

---
+++ src/main/java/com/neomango/neomango/NeomangoApplication.java
@@ -0,0 +1,13 @@
+package com.neomango.neomango;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeomangoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeomangoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/neomango/neomango/NeomangoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neomango.neomango;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeomangoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
